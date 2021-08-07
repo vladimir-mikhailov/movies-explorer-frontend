0e0f0d65756980f855cd9d6db9e4f0c01b194c70
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-08 00:22:49 +0300

    feat: handlers

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,19 +7,20 @@ import Movies from '../movies/Movies/Movies';
 import Profile from '../user/Profile/Profile';
 import ProtectedRoute from '../shared/ProtectedRoute/ProtectedRoute';
 import Register from '../user/Register/Register';
-import { CurrentUserContext } from '../../contexts/CurrentUserContext';
-import { LoggedInContext } from '../../contexts/LoggedInContext';
-// import { IsSavingContext } from '../../contexts/IsSavingContext';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
+import LoggedInContext from '../../contexts/LoggedInContext';
+import IsSavingContext from '../../contexts/IsSavingContext';
+import register from '../../utils/api/user/register';
 import getUser from '../../utils/api/user/getUser';
+// import getUser from '../../utils/api/user/getUser';
 // import updateUser from '../../utils/api/updateUser';
 // import logout from '../../utils/api/logout';
-// import register from '../../utils/api/register';
-// import login from '../../utils/api/login';
+import login from '../../utils/api/user/login';
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const [currentUser, setCurrentUser] = useState({});
-  // const [isSaving, setIsSaving] = useState(false);
+  const [isSaving, setIsSaving] = useState(false);
 
   const isLoggedIn = async () => {
     try {
@@ -40,53 +41,112 @@ function App() {
 
   useEffect(() => isLoggedIn(), []);
 
-  // const handleRegister = () => {
-  //
+  const handleLogin = async ({ email, password }) => {
+    try {
+      setIsSaving(true);
+
+      const user = await login({ email, password });
+
+      if (user) {
+        setIsSaving(false);
+        setCurrentUser(user);
+        setLoggedIn(true);
+      }
+    } catch (e) {
+      setIsSaving(false);
+      setCurrentUser({});
+      setLoggedIn(false);
+      // todo показать ошибку логина
+    }
+
+  };
+
+  const handleRegister = async ({ email, name, password }) => {
+    try {
+      setIsSaving(true);
+
+      const newUser = await register({ email, name, password });
+
+      if (newUser) {
+        setIsSaving(false);
+
+        // todo показать сообщение об успещной регистрации
+
+        await handleLogin({ name, password });
+      }
+    } catch (e) {
+      setIsSaving(false);
+      // todo показать сообщение об ошибке при регистрации
+    }
+  };
+
+  // const handleUpdateUser = async ({ email, name }) => {
+  //  todo handleUpdateUser
   // };
+
+  // const handleLogout = async () => {
+  // todo handleLogout
+  // };
+
+  const closeAllPopups = () => {
+
+  };
+
+  useEffect(() => {
+    const handleEscClose = (e) => {
+      if (e.key === 'Escape') closeAllPopups();
+    };
+
+    document.addEventListener('keydown', handleEscClose);
+  }, []);
 
   return (
     <BrowserRouter>
       <CurrentUserContext.Provider value={currentUser}>
         <LoggedInContext.Provider value={loggedIn}>
-          {/* <IsSavingContext.Provider value={isSaving}> */}
-          <Switch>
-            <Route exact path='/'>
-              <Main />
-            </Route>
+          <IsSavingContext.Provider value={isSaving}>
+            <Switch>
+              <Route exact path='/'>
+                <Main />
+              </Route>
 
-            <Route path='/signup'>
-              {loggedIn && <Redirect to='/' />}
-              <Register
-                // handleRegister={handleRegister}
-                // isSaving={isSaving}
-                isPopup={false}
+              <Route path='/signup'>
+                {loggedIn && <Redirect to='/' />}
+                <Register
+                  handleRegister={handleRegister}
+                  isSaving={isSaving}
+                  isPopup={false}
+                />
+              </Route>
+
+              <Route path='/signin'>
+                {loggedIn && <Redirect to='/' />}
+                <Login
+                  handleLogin={handleLogin}
+                  isSaving={isSaving}
+                  isPopup={false}
+                />
+              </Route>
+
+              <ProtectedRoute
+                path='/profile'
+                component={Profile}
+                loggedIn={loggedIn}
               />
-            </Route>
 
-            <Route path='/signin'>
-              {loggedIn && <Redirect to='/' />}
-              <Login
-                // handleLogin={handleLogin}
-                // isSaving={isSaving}
-                isPopup={false}
+              <ProtectedRoute
+                path='/movies'
+                component={Movies}
+                loggedIn={loggedIn}
               />
-            </Route>
 
-            <ProtectedRoute
-              path='/profile'
-              component={Profile}
-              loggedIn={loggedIn}
-            />
-
-            <ProtectedRoute
-              path='/movies'
-              component={Movies}
-              loggedIn={loggedIn}
-            />
-
-            <ProtectedRoute path='*' component={Profile} loggedIn={loggedIn} />
-          </Switch>
-          {/* </IsSavingContext.Provider> */}
+              <ProtectedRoute
+                path='*'
+                component={Profile}
+                loggedIn={loggedIn}
+              />
+            </Switch>
+          </IsSavingContext.Provider>
         </LoggedInContext.Provider>
       </CurrentUserContext.Provider>
     </BrowserRouter>
