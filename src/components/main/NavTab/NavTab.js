e11a5825f38d7c5a3bdd331d4b6615fbdb0411d0
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-09 01:55:22 +0300

    fix: width

--- src/components/main/NavTab/NavTab.js
+++ src/components/main/NavTab/NavTab.js
@@ -4,7 +4,7 @@ const NavTab = () => (
     <nav className='section navtab'>
       <ul className='section__container navtab__menu-list'>
         <li className='navtab__menu-list-item'>
-          <a className='navtab__menu-link' href='#about-project'>О проекте</a>
+          <a className='navtab__menu-link' href='#about-project'>О&nbsp;проекте</a>
         </li>
         <li className='navtab__menu-list-item'>
           <a className='navtab__menu-link' href='#tech'>Технологии</a>
