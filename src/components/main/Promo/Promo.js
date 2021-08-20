b687cf60432b31b3c416bc3ed364c6020c625a77
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-20 23:53:06 +0300

    fix: Promo section -> div

--- src/components/main/Promo/Promo.js
+++ src/components/main/Promo/Promo.js
@@ -1,10 +1,10 @@
 import './Promo.css';
 
 const Promo = () => (
-      <section className='promo'>
+      <div className='promo'>
         <div className='promo__logo'/>
         <h1 className='promo__heading'>Учебный проект студента факультета Веб-разработки.</h1>
-      </section>
+      </div>
   );
 
 export default Promo;
