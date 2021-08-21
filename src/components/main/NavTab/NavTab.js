93f8cc97ae66b7a709b4a7f50cf3e7a202ca9539
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-21 21:00:28 +0300

    feat: NavTab smooth scroll

--- src/components/main/NavTab/NavTab.js
+++ src/components/main/NavTab/NavTab.js
@@ -1,19 +1,44 @@
+import { Link } from 'react-scroll';
 import './NavTab.css';
 
 const NavTab = () => (
-    <nav className='section navtab'>
-      <ul className='section__container section__container_wide navtab__menu-list'>
-        <li className='navtab__menu-list-item'>
-          <a className='link navtab__menu-link' href='#about-project'>О&nbsp;проекте</a>
-        </li>
-        <li className='navtab__menu-list-item'>
-          <a className='link navtab__menu-link' href='#techs'>Технологии</a>
-        </li>
-        <li className='navtab__menu-list-item'>
-          <a className='link navtab__menu-link' href='#about-me'>Студент</a>
-        </li>
-      </ul>
-    </nav>
-  );
+  <nav className='section navtab'>
+    <ul className='section__container section__container_wide navtab__menu-list'>
+      <li className='navtab__menu-list-item'>
+        <Link
+          className='link navtab__menu-link'
+          to='about-project'
+          href='#about-project'
+          smooth
+          duration={500}
+        >
+          О&nbsp;проекте
+        </Link>
+      </li>
+      <li className='navtab__menu-list-item'>
+        <Link
+          className='link navtab__menu-link'
+          to='techs'
+          href='#techs'
+          smooth
+          duration={500}
+        >
+          Технологии
+        </Link>
+      </li>
+      <li className='navtab__menu-list-item'>
+        <Link
+          className='link navtab__menu-link'
+          to='about-me'
+          href='#about-me'
+          smooth
+          duration={500}
+        >
+          Студент
+        </Link>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default NavTab;
