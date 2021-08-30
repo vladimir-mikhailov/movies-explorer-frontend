3c8b0392fc6e8d93d6f19da4b871a100dbfea3d1
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-30 15:05:07 +0300

    feat: SavedMovies

--- src/components/movies/SavedMovies/SavedMovies.js
+++ src/components/movies/SavedMovies/SavedMovies.js
@@ -1,12 +1,56 @@
-import { useEffect } from 'react';
 import './SavedMovies.css';
+import Header from '../../common/Header/Header';
+import SearchForm from '../SearchForm/SearchForm';
+import Preloader from '../../common/Preloader/Preloader';
+import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Footer from '../../common/Footer/Footer';
 
-const SavedMovies = () => {
-  useEffect(() => {
+const SavedMovies = ({
+  isLoading,
+  movies,
+  handleSearch,
+  handleSave,
+  checkIfSavedAndGetId,
+  setShortsOnly,
+  shortsOnly,
+  searchQuery,
+}) => (
+  <>
+    <Header inHero={false} />
+    <main className='main'>
+      <section className='section'>
+        <div className='section__container section__container_wide'>
+          <SearchForm
+            placeholder='Фильм'
+            handleSubmit={handleSearch}
+            setShortsOnly={setShortsOnly}
+            shortsOnly={shortsOnly}
+            searchQuery={searchQuery}
+          />
+        </div>
+      </section>
 
-  }, []);
-
-  return <></>;
-};
+      {isLoading || movies.length === 0 ? (
+        <section className='section saved-movies saved-movies_preloader'>
+          <div className='section__container section__container_wide'>
+            {isLoading ? <Preloader /> : 'Ничего не найдено'}
+          </div>
+        </section>
+      ) : (
+        <section className='section saved-movies'>
+          <div className='section__container section__container_wide saved-movies__container'>
+            <MoviesCardList
+              movies={movies}
+              handleSave={handleSave}
+              typeSaved
+              checkIfSavedAndGetId={checkIfSavedAndGetId}
+            />
+          </div>
+        </section>
+      )}
+    </main>
+    <Footer />
+  </>
+);
 
 export default SavedMovies;
