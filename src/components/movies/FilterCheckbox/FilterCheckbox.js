ae8ace2e4ca278b020479cda435bb524e2b6d8d7
Vladimir Mikhailov <vladimir@mikhailov.in>
2021-08-30 15:04:04 +0300

    feat: FilterCheckbox

--- src/components/movies/FilterCheckbox/FilterCheckbox.js
+++ src/components/movies/FilterCheckbox/FilterCheckbox.js
@@ -1,17 +1,25 @@
 import './FilterCheckbox.css';
 
-const FilterCheckbox = ({ label }) => (
-  <>
-    <label className='filter-checkbox' htmlFor='shorts'>
-      <input
-        id='shorts'
-        type='checkbox'
-        className='filter-checkbox__control filter-checkbox__control_invisible'
-      />
-      <span className="filter-checkbox__control filter-checkbox__control_pseudo" />
-      {label}
-    </label>
-  </>
-);
+const FilterCheckbox = ({ label, shortsOnly, setShortsOnly }) => {
+  const handleClick = () => {
+    setShortsOnly(!shortsOnly);
+  };
+
+  return (
+    <>
+      <label className='filter-checkbox' htmlFor='shorts'>
+        <input
+          id='shorts'
+          type='checkbox'
+          className='filter-checkbox__control filter-checkbox__control_invisible'
+          checked={shortsOnly}
+          onChange={handleClick}
+        />
+        <span className='filter-checkbox__control filter-checkbox__control_pseudo' />
+        {label}
+      </label>
+    </>
+  );
+};
 
 export default FilterCheckbox;
